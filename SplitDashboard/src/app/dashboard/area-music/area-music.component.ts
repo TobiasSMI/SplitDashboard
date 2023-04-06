e5e0cb488eb8cef82a40e9cc11021c189f54843e
TobiasSMI <41923750+TobiasSMI@users.noreply.github.com>
2023-04-07 00:05:14 +0200

    add video player with study playlist

--- SplitDashboard/src/app/dashboard/area-music/area-music.component.ts
+++ SplitDashboard/src/app/dashboard/area-music/area-music.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-area-music',
   templateUrl: './area-music.component.html',
   styleUrls: ['./area-music.component.scss']
 })
-export class AreaMusicComponent {
+export class AreaMusicComponent implements OnInit {
+  videoUrl: string = 'https://www.youtube.com/embed/videoseries?list=PLfuNlif8uy4OlCgAQYaMyAL1knKLgXnAp';
+  sanitizedUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.sanitizedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoUrl);
+  }
+
+  ngOnInit(): void {}
 }
