5aecf2223a1227d4a02382b722718a3bfc48dea4
TobiasSMI <41923750+TobiasSMI@users.noreply.github.com>
2023-04-07 01:36:53 +0200

    style todo list and make todo lines clearly distinctive to each other

--- SplitDashboard/src/app/dashboard/area-todo/area-todo.component.ts
+++ SplitDashboard/src/app/dashboard/area-todo/area-todo.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface TodoItem {
+  text: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-area-todo',
   templateUrl: './area-todo.component.html',
   styleUrls: ['./area-todo.component.scss']
 })
-export class AreaTodoComponent {
+export class AreaTodoComponent implements OnInit {
+
+  todoItems: TodoItem[] = [
+    {text: 'add further todos', completed: false}
+  ];
+
+  newItem: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addItem() {
+    if (this.newItem !== '') {
+      this.todoItems.push({text: this.newItem, completed: false});
+      this.newItem = '';
+    }
+  }
+
+  removeItem(item: TodoItem) {
+    const index = this.todoItems.indexOf(item);
+    if (index >= 0) {
+      this.todoItems.splice(index, 1);
+    }
+  }
 
 }
